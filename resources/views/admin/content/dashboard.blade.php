2bf75c5a6392873df0bfa9334364f2a25f2a6bdf
Jagdish Rajai <jagdishcumulative@gmail.com>
2026-04-19 20:22:05 +0530

    changes

--- resources/views/admin/content/dashboard.blade.php
+++ resources/views/admin/content/dashboard.blade.php
@@ -16,12 +16,6 @@
                                 <p class="mb-4">Your portfolio CMS is ready. Manage your content below.</p>
                             </div>
                         </div>
-                        <div class="col-sm-5 text-center text-sm-left">
-                            <div class="card-body pb-0 px-0 px-md-4">
-                                <img src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}" height="140"
-                                    alt="Dashboard">
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
