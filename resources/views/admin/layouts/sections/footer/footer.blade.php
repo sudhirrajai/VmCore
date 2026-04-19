2ba4038c4f1bf8ac603dfdbce0d4cc0ff5c418a1
Jagdish Rajai <jagdishcumulative@gmail.com>
2026-04-19 19:42:47 +0530

    changes

--- resources/views/admin/layouts/sections/footer/footer.blade.php
+++ resources/views/admin/layouts/sections/footer/footer.blade.php
@@ -1,9 +1,9 @@
 @php
-$containerFooter = !empty($containerNav) ? $containerNav : 'container-fluid';
+    $containerFooter = !empty($containerNav) ? $containerNav : 'container-fluid';
 @endphp
 
 <!-- Footer-->
-<footer class="content-footer footer bg-footer-theme">
+<!-- <footer class="content-footer footer bg-footer-theme">
     <div class="{{ $containerFooter }}">
         <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
             <div class="text-body">
@@ -20,5 +20,5 @@ $containerFooter = !empty($containerNav) ? $containerNav : 'container-fluid';
             </div>
         </div>
     </div>
-</footer>
+</footer> -->
 <!--/ Footer-->
